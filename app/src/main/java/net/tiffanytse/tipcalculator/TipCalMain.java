74a014885b15212df32a35063cd5a62d6dbaf50b
Andy <tse_andy@student.smc.edu>
2016-05-07 13:20:06 -0700

    first commit

---
+++ app/src/main/java/net/tiffanytse/tipcalculator/TipCalMain.java
@@ -0,0 +1,13 @@
+package net.tiffanytse.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipCalMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_cal_main);
+    }
+}
