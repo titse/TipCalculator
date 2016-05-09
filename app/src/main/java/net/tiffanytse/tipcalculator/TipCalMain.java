4134e4493949dfd89076d0b4c547135f453d0398
Tiffany <ttse93@gmail.com>
2016-05-08 21:10:22 -0700

    change back to some original code

--- app/src/main/java/net/tiffanytse/tipcalculator/TipCalMain.java
+++ app/src/main/java/net/tiffanytse/tipcalculator/TipCalMain.java
@@ -17,9 +17,9 @@ public class TipCalMain extends AppCompatActivity {
     private double currentBillTotal; //bill amount entered by the user
     private int currentCustomPercent; //tip % set with the SeekBar
     private EditText tip10EditText; //displays 10% tip
-    private EditText total0EditText; //displays total with 10% tip
+    private EditText total10EditText; //displays total with 10% tip
     private EditText tip15EditText; //displays 15% tip
-    private EditText total5EditText; //displays total with 15% tip
+    private EditText total15EditText; //displays total with 15% tip
     private EditText tip20EditText; //displays 20% tip
     private EditText total20EditText; //displays total with 20% tip
     private TextView customTipTextView; //displays custom tip percentage
@@ -37,8 +37,21 @@ public class TipCalMain extends AppCompatActivity {
             currentBillTotal =0.0; //initalize the bill amount to zero
             currentCustomPercent = 18; // intilize the custome tip to 18%
         }
-        else { //app
+        else { //app is being restored from memory, not executed from scratch
+            //intialize the bill amount to saved amount
+            currentBillTotal = savedInstanceState.getDouble(BILL_TOTAL);
+            //initilize the custom tip to save tip percent
+            currentCustomPercent =savedInstanceState.getInt(CUSTOM_PERCENT);
 
         }
+
+        //get references to the 10%, 15%, 20% tip and total Edit
+        tip10EditText = (EditText) findViewById(R.id.tip10EditText);
+        total10EditText = (EditText) findViewById(R.id.total10EditText);
+        tip15EditText = (EditText) findViewById(R.id.tip15EditText);
+        total15EditText = (EditText) findViewById(R.id.total15EditText);
+        tip20EditText = (EditText) findViewById(R.id.tip20EditText);
+        total20EditText = (EditText) findViewById(R.id.total20EditText);
+
     }
 }
